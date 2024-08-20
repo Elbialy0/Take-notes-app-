a742379f76b3ec1f0891af4cb676f88c50b2a815
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2024-08-20 18:39:21 +0300

    Setup the app

---
+++ V1/src/test/java/com/example/takeNotesApp/TakeNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takeNotesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeNotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
